65344314698f9f5bdc23f09986277f4f86d857f7
Francis Kinyae <fmkinyae@gmail.com>
2021-09-09 15:24:21 +0300

    add user model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    is_cutomer = models.BooleanField('customer status', default=False)
+    is_agent = models.BooleanField('agent status', default=False)
+

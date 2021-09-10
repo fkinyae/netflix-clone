82c7f66579eb3b713a30d294cf7f9fe107829b10
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-10 16:59:07 +0300

    Update of forms.py file

--- movies/form.py
+++ movies/form.py
@@ -6,16 +6,15 @@ from django.db import transaction
 from .models import User, Customer, Agent
 
 
- 
 class CustomerSignUpForm(UserCreationForm):
-   first_name = forms.CharField(required=True)
-   last_name = forms.CharField(required=True)
-   email = forms.EmailField(required=True)
-   phone_number = forms.CharField(required=False)
-   Address = forms.CharField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    phone_number = forms.CharField(required=False)
+    Address = forms.CharField(required=True)
 
-class Meta(UserCreationForm.Meta):
-    model = User
+    class Meta(UserCreationForm.Meta):
+        model = User
 
     @transaction.atomic
     def save(self):
@@ -27,31 +26,32 @@ class Meta(UserCreationForm.Meta):
         user.save()
         customer = Customer.objects.create(user=user)
         customer.phone_number = self.cleaned_data.get('phone_number')
-        customer.Address = self.cleaned_data.get('location')
+        customer.address = self.cleaned_data.get('address')
         customer.save()
         return user
+
 
 class AgentSignUpForm(UserCreationForm):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
     phone_number = forms.CharField(required=False)
-    Address = forms.CharField(required=True)
+    address = forms.CharField(required=True)
 
     class Meta(UserCreationForm.Meta):
-            model = User
+        model = User
 
     @transaction.atomic
     def save(self):
         user = super().save(commit=False)
-        user.is_employee = True
-        user.is_staff = True
+        user.is_customer = True
+        user.is_agent = True
         user.first_name = self.cleaned_data.get('first_name')
         user.last_name = self.cleaned_data.get('last_name')
         user.email = self.cleaned_data.get('email')
         user.save()
-        employee = Employee.objects.create(user=user)
-        employee.phone_number = self.cleaned_data.get('phone_number')
-        employee.designation = self.cleaned_data.get('designation')
-        employee.save()
+        agent = Agent.objects.create(user=user)
+        agent.phone_number = self.cleaned_data.get('phone_number')
+        agent.address = self.cleaned_data.get('address')
+        agent.save()
         return user

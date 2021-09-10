b1a7ce8a979bfbe7312ce08d6634ab18dfa2b615
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-10 16:42:54 +0300

    this add a module customer sign up

--- movies/form.py
+++ movies/form.py
@@ -5,3 +5,14 @@ from django import forms
 from django.db import transaction
 from .models import User, Customer, Agent
 
+
+ 
+class CustomerSignUpForm(UserCreationForm):
+ 
+   first_name = forms.CharField(required=True)
+   last_name = forms.CharField(required=True)
+   email = forms.EmailField(required=True)
+   phone_number = forms.CharField(required=False)
+   location = forms.CharField(required=True)
+
+

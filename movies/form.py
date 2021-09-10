0e6a0609a9b99b5b809a39559310e3d615a2c5b2
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-10 16:45:48 +0300

    this adds a class meta that saves the fields

--- movies/form.py
+++ movies/form.py
@@ -15,4 +15,23 @@ class CustomerSignUpForm(UserCreationForm):
    phone_number = forms.CharField(required=False)
    location = forms.CharField(required=True)
 
+class Meta(UserCreationForm.Meta):
+    model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_customer = True
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')
+        user.email = self.cleaned_data.get('email')
+        user.save()
+        customer = Customer.objects.create(user=user)
+        customer.phone_number = self.cleaned_data.get('phone_number')
+        customer.location = self.cleaned_data.get('location')
+        customer.save()
+        return user
+
+
+
 

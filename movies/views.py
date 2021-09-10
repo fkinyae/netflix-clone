5b11fd4a5d1ab8e53fe3e3c9c73cfee905bb13b1
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-10 16:31:37 +0300

    this adds the login part

--- movies/views.py
+++ movies/views.py
@@ -34,4 +34,22 @@ def form_valid(self, form):
     login(self.request, user)
     return redirect('/')
 
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index/')
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, 'registration/login.html',
+                  context={'form': AuthenticationForm()})
+
+
 

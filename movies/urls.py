3fa197cfa0649459198047c5ed46ddf9c8faced4
Francis Kinyae <fmkinyae@gmail.com>
2021-09-10 13:06:06 +0300

    add url for changing password

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path,include
 from . import views
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
-    path('', views.welcome, name='welcome')
+    path('', views.welcome, name='welcome'),
+    path('login/', views.login_view, name='login'),
+    #path('password/', auth_views.PasswordChangeView.as_view()),
+    path('password_change/', views.PasswordsChangeView.as_view(), name='password_change'),
 ]
